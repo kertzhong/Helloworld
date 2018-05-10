dafb0cfd5b67aa50815b49c65ff8af57706422e6
kertzhong <34335845+kertzhong@users.noreply.github.com>
2018-05-10 15:50:11 +0800

    upload hello.go
    upload the first golang example--Helloworld

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main /*define the package name*/
+import "fmt" //single raw notes
+func main(){
+  fmt.Println("Hello, World!\n I am Kert"); fmt.Println("\nThat me again!")
+  var myage int
+  var mysonage int
+  var sumage int
+  myage=40
+  mysonage=8
+  sumage= myage + mysonage
+  fmt.Println(sumage)
+}
